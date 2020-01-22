e8131b6bce0034ac39d7e6d1c7bb5380a044553d
Ecuki <jeziorski.emil@gmail.com>
2020-01-22 19:18:46 +0100

    second

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,2 @@
-k;
-llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll; // jest-dom adds custom jest matchers for asserting on DOM nodes.////////////
+// jest-dom adds custom jest matchers for asserting on DOM nodes.////////////
 import "@testing-library/jest-dom/extend-expect";
-nnnnnnnnnnnnnnnnnnnnnnnngghg777777765ffvv;
-llllllllllllllllllliiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii,
-  (<p></p>);
